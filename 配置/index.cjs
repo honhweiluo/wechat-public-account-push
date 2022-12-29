61d20028dddb9a5296cc3fa09734a63e2c1dfb3f
honhweiluo <121596582+honhweiluo@users.noreply.github.com>
2022-12-29 20:47:06 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -23,13 +23,13 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ohTsF6Z-71KGyCtUyRCE9W0ASXbw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ohTsF6dVDv3f-8grDCdKFg-RU00A',
+      useTemplateId: 'm7GT-CvYuuykNpwkQwIXmbkmVeADFnjMv4TJBF5sM74',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '1-17',
     
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'm7GT-CvYuuykNpwkQwIXmbkmVeADFnjMv4TJBF5sM74',
 
   CALLBACK_USERS: [
     {
